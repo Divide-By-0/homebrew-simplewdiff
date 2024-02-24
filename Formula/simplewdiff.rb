e683c3823aeb3c99b101bc01046c7b39f881569f
Divide-By-0 <aayushg@mit.edu>
2024-02-23 21:53:15 -0700

    move rb

--- Formula/simplewdiff.rb
+++ Formula/simplewdiff.rb
@@ -1,8 +1,8 @@
 class Simplewdiff < Formula
   desc "Compare files with word-by-word diffs, and omit unchanged lines. Thin wrapper atop wdiff, grep, and colordiff"
-  homepage "https://github.com/yourusername/simplewdiff"
-  url "https://github.com/yourusername/simplewdiff/archive/v1.0.0.tar.gz"
-  sha256 "your_tar_gz_sha256"
+  homepage "https://github.com/Divide-By-0/simplewdiff"
+  url "https://github.com/Divide-By-0/simplewdiff/archive/v1.0.0.tar.gz"
+  sha256 "d0484ccd5614c4762047beb857104e319a51c2234813923253c73a79d556c675"
 
   # Declare dependencies
   depends_on "wdiff"
